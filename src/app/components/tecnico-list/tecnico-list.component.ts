db772d6f70dbe3b67b5e02739cba991d51e1440c
gabrielgua <gabrieljmym13@gmail.com>
2022-06-14 17:12:56 -0300

    html e css para tecnico-list

--- src/app/components/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Gabriel Guaitanele Niszczak',
+      cpf: '12292554941',
+      email: 'gabriel@mail.com',
+      senha: '123123',
+      perfis: ['0'],
+      dataCriacao: '30/06/2020'
+    }
+  ];
+  
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
+
