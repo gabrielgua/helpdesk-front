4c17c59cd5d1ebbe8100ab5521d6e2a9fac8c982
gabrielgua <gabrieljmym13@gmail.com>
2022-06-14 19:53:54 -0300

    autenticacao de login implementada

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,22 +20,25 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router
+    ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   logar() {
-    this.toast.error('Usuário e/ou senha incorretos!', 'Login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+    }, () => {
+      this.toast.error('Usuário e/ou Senha inválidos!', 'Login');
+    })
   }
 
   validaCampos(): boolean {
-    if (this.email.valid && this.senha.valid) {
-      return true;
-    } 
-
-    return false;
+    return this.email.valid && this.senha.valid;
   }
 
 }
