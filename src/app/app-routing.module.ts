9f8451020aa5b074f5f084d62fbd7e0f1c80ed72
gabrielgua <gabrieljmym13@gmail.com>
2022-06-20 16:18:53 -0300

    adicionado rota para tecnico-create e tecnico-list movido para pasta tecnicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
-import { TecnicoListComponent } from './components/tecnico-list/tecnico-list.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent} ,
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
         {path: 'home', component: HomeComponent},
-        {path: 'tecnicos', component: TecnicoListComponent}
+        {path: 'tecnicos', component: TecnicoListComponent},
+        {path: 'tecnicos/create', component: TecnicoCreateComponent},
+        
     ]
   }
 ];
