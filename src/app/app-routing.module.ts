c17c9be384cec416543adc18b4843fe972281e3a
gabrielgua <gabrieljmym13@gmail.com>
2022-07-05 19:02:47 -0300

    rota para chamado-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
-import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent} ,
@@ -27,6 +27,8 @@ const routes: Routes = [
         {path: 'clientes/update/:id', component: ClienteUpdateComponent},
 
         {path: 'chamados', component: ChamadoListComponent},
+        {path: 'chamados/create', component: ChamadoCreateComponent},
+        
     ]
   }
 ];
