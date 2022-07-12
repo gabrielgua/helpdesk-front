a9cafad7600db64f5e83db351aae9f762bcbf83e
gabrielgua <gabrieljmym13@gmail.com>
2022-07-12 17:11:37 -0300

    componente dialog criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 
 
@@ -68,6 +69,7 @@ import { ChamadoUpdateComponent } from './components/chamado/chamado-update/cham
     ChamadoListComponent,
     ChamadoCreateComponent,
     ChamadoUpdateComponent,
+    DialogComponent,
   ],
   imports: [
     BrowserModule,
